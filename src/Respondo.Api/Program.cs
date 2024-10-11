5609138c636fe74f6b0dfd456a99aa753a4d4cac
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-11 19:52:20 +0200

    feat: update Program with configuration

--- src/Respondo.Api/Program.cs
+++ src/Respondo.Api/Program.cs
@@ -1,6 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Text.Json.Serialization;
+using Respondo.Core.Identity.Configuration;
+using Wolverine;
 
-app.MapGet("/", () => "Hello World!");
+namespace Respondo.Api;
 
-app.Run();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true);
+
+        builder.Services.AddControllers()
+            .AddJsonOptions(options => { options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull; });
+
+        builder.Services.AddSingleton(TimeProvider.System);
+
+        builder.ConfigureIdentityModule();
+        
+        builder.UseWolverine(options =>
+        {
+            options.Discovery.DisableConventionalDiscovery();
+
+            options.IncludeIdentityCore(builder.Configuration);
+        });
+        
+        var app = builder.Build();
+
+        app.MapControllers();
+
+        if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+        {
+            app.RunIdentityDbMigrations();
+        }
+        
+        app.Run();
+    }
+}
