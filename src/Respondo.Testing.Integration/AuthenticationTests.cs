cbd160144fabef7506a1cb409bc0df8fc78eb838
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-11 19:53:23 +0200

    tests: add integration tests for AuthenticationController

---
+++ src/Respondo.Testing.Integration/AuthenticationTests.cs
@@ -0,0 +1,64 @@
+using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Respondo.Api;
+using Respondo.Api.ViewModels;
+using Respondo.Core.Identity.Contracts;
+using Respondo.Core.Identity.Contracts.Entities;
+using Respondo.Testing.Integration.Helpers;
+
+namespace Respondo.Testing.Integration;
+
+public class AuthenticationTests(TestFactory<Program> factory) : IClassFixture<TestFactory<Program>>
+{
+    [Fact]
+    public async Task Should_Register_A_New_User()
+    {
+        var client = factory.CreateClientWithoutRedirect();
+        var response = await client.PostAsJsonAsync("/api/Authentication/register", new CreateApplicationUser
+        {
+            Username = "shouldRegister",
+            Email = "shouldRegister@email.com",
+            Password = "shouldRegister1234!"
+        });
+
+        response.StatusCode.Should().Be(HttpStatusCode.Redirect);
+        response.Headers.Should().ContainKey("Set-Cookie");
+    }
+
+    [Fact]
+    public async Task Should_Login_An_Existing_User()
+    {
+        #region Setup
+
+        var manager = factory.Services.GetService<UserManager<ApplicationUser>>();
+
+        var user = new ApplicationUser
+        {
+            Id = Guid.CreateVersion7(DateTimeOffset.UtcNow).ToString(),
+            UserName = "shouldLogin",
+            Email = "shouldLogin@email.com"
+        };
+
+        await manager?.CreateAsync(user, "shouldLogin1234!")!;
+
+        #endregion
+        
+        var client = factory.CreateClientWithoutRedirect();
+        
+        var response = await client.PostAsJsonAsync("/api/Authentication/login", new LoginViewModel
+        {
+            Email = "shouldLogin@email.com",
+            Password = "shouldLogin1234!"
+        });
+        
+        response.StatusCode.Should().Be(HttpStatusCode.Redirect);
+        response.Headers.Should().ContainKey("Set-Cookie");
+
+        response = await client.GetAsync("api/Authentication/authenticate");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
---
+++ src/Respondo.Testing.Integration/Helpers/Extensions.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Respondo.Testing.Integration.Helpers;
+
+public static class Extensions
+{
+    public static void RemoveDbContext<T>(this IServiceCollection services) where T : DbContext
+    {
+        var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<T>));
+        if (descriptor != null) services.Remove(descriptor);
+    }
+
+    public static void EnsureDbCreated<T>(this IServiceCollection services) where T : DbContext
+    {
+        var serviceProvider = services.BuildServiceProvider();
+
+        using var scope = serviceProvider.CreateScope();
+        var scopedServices = scope.ServiceProvider;
+        var context = scopedServices.GetRequiredService<T>();
+
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+    }
+}
