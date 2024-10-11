595077be9b8299e79f7e716550ab17f7ead039c6
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-11 22:29:07 +0200

    feat: implement CreateOccasion handler

---
+++ src/Respondo.Core.Occasions/CreateOccasionHandler.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Respondo.Core.Occasions.Persistence;
+using Respondo.Core.Occasions.Contracts;
+using Respondo.Core.Occasions.Contracts.Entities;
+
+namespace Respondo.Core.Occasions;
+
+public sealed record CreateOccasionHandler
+{
+    public async Task<(CreateOccasionResponse, OccasionCreated)> Handle(CreateOccasion request, OccasionDbContext context)
+    {
+        var profile = await context.Profiles.FirstAsync(profile => profile.Id == request.ProfileId);
+
+        var occasion = new Occasion
+        {
+            Id = Guid.CreateVersion7(TimeProvider.System.GetUtcNow()),
+            Name = request.Name,
+            Profile = profile
+        };
+
+        await context.Occasions.AddAsync(occasion);
+
+        await context.SaveChangesAsync();
+
+        return (new CreateOccasionResponse { Id = occasion.Id }, new OccasionCreated { Id = occasion.Id });
+    }
+}
