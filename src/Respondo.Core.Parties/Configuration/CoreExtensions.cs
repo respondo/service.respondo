3ecd3e8b2d62baa99666558f52c621c9aa1767f3
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-13 21:06:16 +0200

    feat: implement CoreExtensions for Parties module

---
+++ src/Respondo.Core.Parties/Configuration/CoreExtensions.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Respondo.Core.Parties.Persistence;
+using Wolverine;
+
+namespace Respondo.Core.Parties.Configuration;
+
+/// <summary>
+///     Extensions for configuring the Parties Core Module
+/// </summary>
+public static class CoreExtensions
+{
+    /// <summary>
+    ///     Configure the Parties Core Module.     
+    /// </summary>
+    /// <param name="builder"><see cref="WebApplicationBuilder"/>.</param>
+    public static void ConfigurePartiesModule(this WebApplicationBuilder builder)
+    {
+        var identityDbConnectionString = builder.Configuration.GetConnectionString("OccasionsDb");
+
+        builder.Services.AddDbContext<PartiesDbContext>(options =>
+        {
+            options.UseNpgsql(identityDbConnectionString, optionsBuilder =>
+            {
+                optionsBuilder.MigrationsAssembly(typeof(PartiesDbContext).Assembly.FullName);
+                optionsBuilder.UseNodaTime();
+            });
+        });
+    }
+
+    /// <summary>
+    ///     Run migrations for <see cref="PartiesDbContext"/>.
+    /// </summary>
+    /// <param name="app"><see cref="WebApplication"/>.</param>
+    public static void RunPartiesDbMigrations(this WebApplication app)
+    {
+        using var serviceScope = app.Services.GetService<IServiceScopeFactory>()?.CreateScope();
+        serviceScope?.ServiceProvider.GetRequiredService<PartiesDbContext>().Database.Migrate();
+    }
+    
+    /// <summary>
+    ///     Include Wolverine handlers for Parties Core operations.
+    /// </summary>
+    /// <param name="options"><see cref="WolverineOptions"/>.</param>
+    /// <param name="configuration"><see cref="IConfiguration"/>.</param>
+    public static void IncludePartiesModule(this WolverineOptions options, IConfiguration configuration)
+    {
+    }
+}
