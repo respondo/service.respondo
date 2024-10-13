6b0a057dcd9e1b338e4426137ded28f247ed5228
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-13 20:42:23 +0200

    feat: create PartiesDbContext

---
+++ src/Respondo.Core.Parties/Persistence/PartiesDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Respondo.Core.Parties.Entities;
+
+namespace Respondo.Core.Parties.Persistence;
+
+internal class PartiesDbContext(DbContextOptions<PartiesDbContext> options)
+    : DbContext(options)
+{
+    public required DbSet<Member> Members { get; init; }
+    public required DbSet<Occasion> Occasions { get; init; }
+    public required DbSet<Party> Parties { get; init; }
+    public required DbSet<Profile> Profiles { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(PartiesDbContext).Assembly);
+    }
+}
