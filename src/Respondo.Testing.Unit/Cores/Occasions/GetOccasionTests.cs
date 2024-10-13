fbbb10ca8f4e24a48cf6b28d6de1d5337a3735f3
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-13 18:54:06 +0200

    tests: add tests for GetOccasion

---
+++ src/Respondo.Testing.Unit/Cores/Occasions/GetOccasionTests.cs
@@ -0,0 +1,85 @@
+using FluentAssertions;
+using Respondo.Core.Occasions;
+using Respondo.Core.Occasions.Contracts;
+using Respondo.Core.Occasions.Entities;
+using Respondo.Core.Occasions.Persistence;
+using Respondo.Testing.Unit.Helpers;
+
+namespace Respondo.Testing.Unit.Cores.Occasions;
+
+public class GetOccasionTests(DbContextFixture<OccasionDbContext> dbFixture) : IClassFixture<DbContextFixture<OccasionDbContext>>
+{
+    [Fact]
+    public async Task Should_Get_Occasion()
+    {
+        #region Setup
+     
+        var profile = new Profile
+        {
+            Id = Guid.NewGuid()
+        };
+
+        await dbFixture.DbContext.Profiles.AddAsync(profile);
+
+        var occasionId = Guid.NewGuid();
+
+        await dbFixture.DbContext.Occasions.AddAsync(new Occasion
+        {
+            Id = occasionId,
+            Name = "ShouldGetOccasion",
+            Profile = profile
+        });
+
+        await dbFixture.DbContext.SaveChangesAsync();
+
+        #endregion
+
+        var request = new GetOccasion
+        {
+            Id = occasionId,
+            ProfileId = profile.Id
+        };
+        
+        var result = await new GetOccasionHandler().Handle(request, dbFixture.DbContext);
+
+        result.Should().NotBeNull();
+        result!.Id.Should().Be(occasionId);
+        result.Name.Should().Be("ShouldGetOccasion");
+    }
+
+    [Fact]
+    public async Task Should_Not_Get_Occasion_When_Profile_Is_Different()
+    {
+        #region Setup
+     
+        var profile = new Profile
+        {
+            Id = Guid.NewGuid()
+        };
+
+        await dbFixture.DbContext.Profiles.AddAsync(profile);
+
+        var occasionId = Guid.NewGuid();
+
+        await dbFixture.DbContext.Occasions.AddAsync(new Occasion
+        {
+            Id = occasionId,
+            Name = "ShouldGetOccasion",
+            Profile = profile
+        });
+
+        await dbFixture.DbContext.SaveChangesAsync();
+
+        #endregion
+
+        var request = new GetOccasion
+        {
+            Id = occasionId,
+            ProfileId = Guid.NewGuid()
+        };
+        
+        var result = await new GetOccasionHandler().Handle(request, dbFixture.DbContext);
+
+        result.Should().BeNull();
+    }
+}
