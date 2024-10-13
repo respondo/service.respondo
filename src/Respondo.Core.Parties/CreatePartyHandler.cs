a011bcbcdae0053ba5eb45093ffd9de9528fbe38
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-13 21:10:57 +0200

    feat: add handler for CreateParty

---
+++ src/Respondo.Core.Parties/CreatePartyHandler.cs
@@ -0,0 +1,12 @@
+using Respondo.Core.Parties.Contracts;
+using Wolverine;
+
+namespace Respondo.Core.Parties;
+
+public sealed record CreatePartyHandler
+{
+    public Task Handle(CreateParty request, Envelope envelope)
+    {
+        throw new NotImplementedException();
+    }
+}
