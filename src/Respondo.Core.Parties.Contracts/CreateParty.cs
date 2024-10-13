7aa8ae9c7caf7e9b3315410f463a900f45fb7a1d
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-13 21:26:47 +0200

    feat: update CreateParty

--- src/Respondo.Core.Parties.Contracts/CreateParty.cs
+++ src/Respondo.Core.Parties.Contracts/CreateParty.cs
@@ -1,8 +1,9 @@
 namespace Respondo.Core.Parties.Contracts;
 
-public record CreateParty
+public sealed record CreateParty
 {
     public required Guid OccasionId { get; init; }
+    public Guid ProfileId { get; init; }
     public required string Name { get; init; }
     public string? Email { get; init; }
 }
