a4488e4898d69d5064cc67d7098190137c6aec7f
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-14 13:09:45 +0200

    feat: implement handler for GetParty

--- src/Respondo.Core.Parties/GetPartyHandler.cs
+++ src/Respondo.Core.Parties/GetPartyHandler.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Respondo.Core.Parties.Contracts;
 using Respondo.Core.Parties.Persistence;
 
@@ -7,6 +8,22 @@ public sealed record GetPartyHandler
 {
     public async Task<GetPartyResponse?> Handle(GetParty request, PartiesDbContext context)
     {
-        throw new NotImplementedException();
+        
+        var party = context.Parties
+            .Where(party => party.Occasion.Profile.Id == request.ProfileId)
+            .Where(party => party.Id == request.Id)
+            .Select(party => new GetPartyResponse
+            {
+                Id = party.Id,
+                Name = party.Name,
+                Email = party.Email,
+                Members = party.Members.Select(member => new GetPartyResponse.Member
+                {
+                    Id = member.Id,
+                    Name = member.Name,
+                }).ToList()
+            });
+
+        return await party.FirstOrDefaultAsync();
     }
 }
