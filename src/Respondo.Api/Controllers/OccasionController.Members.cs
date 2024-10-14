4d96fa058470215c0509f5182603bbf6ad182e5f
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-14 16:23:28 +0200

    feat: add AddMemberToParty request to controllers

---
+++ src/Respondo.Api/Controllers/OccasionController.Members.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Respondo.Api.Extensions;
+using Respondo.Api.Models.Party;
+using Respondo.Core.Parties.Contracts;
+
+namespace Respondo.Api.Controllers;
+
+public partial class OccasionController
+{
+    [HttpPost("{occasionId:guid}/party{partyId:guid}/member")]
+    public async Task<IActionResult> AddMemberToParty([FromRoute] Guid occasionId, [FromRoute] Guid partyId,
+        [FromBody] AddMemberToPartyModel model, CancellationToken cancellationToken)
+    {
+        var request = model.ToRequest(partyId, User.GetProfileId());
+
+        await _bus.InvokeAsync(request, cancellationToken);
+
+        return Accepted();
+    }
+}
