916c29b80f8552666e2120ca16a475dd67c0c4e1
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-15 21:25:39 +0200

    tests: add integration tests for deleting party member

--- src/Respondo.Testing.Integration/MemberTests.cs
+++ src/Respondo.Testing.Integration/MemberTests.cs
@@ -66,4 +66,62 @@ public class MemberTests(TestFactory<Program> factory) : IClassFixture<TestFacto
         party.Should().NotBeNull();
         party!.Members.Should().Contain(m => m.Name == "New Member");
     }
+    
+    [Fact]
+    public async Task Should_Delete_Member_From_Party()
+    {
+        #region Setup
+
+        var client = factory.CreateClientWithoutRedirect();
+        await client.PostAsJsonAsync("/api/Authentication/register", new RegisterModel
+        {
+            Username = "Should_Delete_Member_From_Party",
+            Email = "Should_Delete_Member_From_Party@email.com",
+            Password = "password1234!"
+        });
+
+        var occasionCreationResponse = await client.PostAsJsonAsync("/api/Occasion", new CreateOccasionModel
+        {
+            Name = "Should_Delete_Member_From_Party"
+        });
+
+        var occasionCreationResponseContent = await occasionCreationResponse.Content.ReadFromJsonAsync<CreateOccasionResponse>();
+        var occasionId = occasionCreationResponseContent!.Id;
+
+        var partyCreationResponse = await client.PostAsJsonAsync($"api/Occasion/{occasionId}/party", new CreatePartyModel
+        {
+            Name = "Should_Delete_Member_From_Party",
+            Email = "Should_Delete_Member_From_Party@email.com"
+        });
+
+        var partyCreationResponseContent = await partyCreationResponse.Content.ReadFromJsonAsync<CreatePartyResponse>();
+        var partyId = partyCreationResponseContent!.Id;
+
+        var addMemberResponse = await client.PostAsJsonAsync($"api/Occasion/{occasionId}/party/{partyId}/member", new AddPartyMemberRequest
+        {
+            Name = "New Member"
+        });
+
+        addMemberResponse.StatusCode.Should().Be(HttpStatusCode.Accepted);
+
+        await Task.Delay(1000);
+
+        var partyInfo = await client.GetFromJsonAsync<GetPartyResponse>($"api/Occasion/{occasionId}/party/{partyId}");
+
+        #endregion
+        
+        var response = await client.DeleteAsync($"api/Occasion/{occasionId}/party/{partyId}/member/{partyInfo!.Members[0].Id}");
+        
+        response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+        
+        await Task.Delay(1000);
+        
+        var dbContext = factory.Services.GetRequiredService<PartiesDbContext>();
+        
+        var party = await dbContext.Parties
+            .Include(p => p.Members)
+            .FirstAsync(p => p.Id == partyId);
+        
+        party.Members.Should().BeEmpty();
+    }
 }
