4bfdd2d26d2038f9f95a49699e44e00f6bbd5070
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-15 15:56:38 +0200

    feat: implement handler for DeleteParty

--- src/Respondo.Core.Parties/DeletePartyHandler.cs
+++ src/Respondo.Core.Parties/DeletePartyHandler.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Respondo.Core.Parties.Contracts;
 using Respondo.Core.Parties.Persistence;
 
@@ -5,8 +6,20 @@ namespace Respondo.Core.Parties;
 
 public sealed record DeletePartyHandler
 {
-    public Task<PartyDeleted?> Handle(DeleteParty request, PartiesDbContext context)
+    public async Task<PartyDeleted?> Handle(DeleteParty request, PartiesDbContext context)
     {
-        throw new NotImplementedException();
+        var party = await context.Parties
+            .Where(party => party.Occasion.Profile.Id == request.ProfileId)
+            .FirstOrDefaultAsync(party => party.Id == request.Id);
+
+        if (party is null)
+        {
+            return default;
+        }
+
+        context.Parties.Remove(party);
+        await context.SaveChangesAsync();
+
+        return new PartyDeleted { Id = party.Id };
     }
 }
