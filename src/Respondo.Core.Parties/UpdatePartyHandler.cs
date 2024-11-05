54b7013936e32906c7bbfd01cf8dfecb59f842bd
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-11-05 22:28:51 +0100

    feat: implement handler for UpdateParty

---
+++ src/Respondo.Core.Parties/UpdatePartyHandler.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Respondo.Core.Parties.Contracts;
+using Respondo.Core.Parties.Entities;
+using Respondo.Core.Parties.Persistence;
+using Wolverine;
+
+namespace Respondo.Core.Parties;
+
+public sealed record UpdatePartyHandler
+{
+    public async Task<PartyUpdated?> Handle(UpdateParty request, PartiesDbContext context, ILogger<UpdatePartyHandler> logger)
+    {
+        var party = await context.Parties
+            .Where(party => party.Occasion.Profile.Id == request.ProfileId)
+            .FirstOrDefaultAsync(party => party.Id == request.PartyId);
+
+        if (party is null)
+        {
+            logger.LogError("Party with id {PartyId} not found but update was requested", request.PartyId);
+            
+            return default;
+        }
+
+        if (request.Name is { Length: > 0 } name)
+        {
+            party.Name = name;
+        }
+        
+        if (request.Email is { Length: > 0 } email)
+        {
+            party.Email = email;
+        }
+
+        if (context.ChangeTracker.HasChanges())
+        {
+            await context.SaveChangesAsync();
+            
+            return new PartyUpdated { Id = party.Id };
+        }
+
+        return default;
+    }
+}
