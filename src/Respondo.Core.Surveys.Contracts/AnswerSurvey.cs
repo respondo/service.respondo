2842bd37889c5bcbe5f106b44499fedc508a71e1
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-11-09 18:39:00 +0100

    chore: correct some stuff

--- src/Respondo.Core.Surveys.Contracts/AnswerSurvey.cs
+++ src/Respondo.Core.Surveys.Contracts/AnswerSurvey.cs
@@ -5,5 +5,5 @@ public sealed record AnswerSurvey
     public required Guid SurveyId { get; init; }
     public required Guid PartyId { get; init; }
     
-    public required List<(Guid MemberId, (Guid QuestionId, string Answer)[])> AnswersByMember { get; init; }
+    public required List<(Guid MemberId, (Guid QuestionId, string? Answer)[])> AnswersByMember { get; init; }
 }
