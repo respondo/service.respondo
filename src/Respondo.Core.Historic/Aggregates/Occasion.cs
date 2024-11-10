5f7c7947ea4b37eea8c59f45ae7b6e67ca696018
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-11-10 17:59:40 +0100

    feat(historic): expand Occasion aggregate with new properties

--- src/Respondo.Core.Historic/Aggregates/Occasion.cs
+++ src/Respondo.Core.Historic/Aggregates/Occasion.cs
@@ -1,8 +1,20 @@
 namespace Respondo.Core.Historic.Aggregates;
 
-public record Occasion()
+public sealed record Occasion()
 {
     public required Guid Id { get; init; }
+    public required string Name { get; init; }
+    public  List<Party> Parties { get; init; } = [];
     
     public required DateTimeOffset CreatedAt { get; init; }
+    public required DateTimeOffset UpdatedAt { get; init; }
+
+    public sealed record Party()
+    {
+        public required Guid Id { get; init; }
+        public required string Name { get; init; }
+        
+        public required DateTimeOffset CreatedAt { get; init; }
+        public required DateTimeOffset UpdatedAt { get; init; }
+    }
 }
--- src/Respondo.Core.Historic/Aggregates/OccasionProjection.cs
+++ src/Respondo.Core.Historic/Aggregates/OccasionProjection.cs
@@ -1,6 +1,7 @@
 using Marten.Events;
 using Marten.Events.Aggregation;
 using Respondo.Core.Occasions.Contracts;
+using Respondo.Core.Parties.Contracts;
 
 namespace Respondo.Core.Historic.Aggregates;
 
@@ -11,7 +12,53 @@ public class OccasionProjection : SingleStreamProjection<Occasion>
         return new Occasion
         {
             Id = @event.Data.OccasionId,
-            CreatedAt = @event.Timestamp
+            Name = @event.Data.Name,
+            CreatedAt = @event.Timestamp,
+            UpdatedAt = @event.Timestamp
         };
     }
+
+    #region Parties Core
+
+    public Occasion Apply(IEvent<PartyCreated> @event, Occasion state)
+    {
+        state.Parties.Add(new Occasion.Party
+        {
+            Id = @event.Data.PartyId,
+            Name = @event.Data.Name,
+            CreatedAt = @event.Timestamp,
+            UpdatedAt = @event.Timestamp
+        });
+        
+        return state with
+        {
+            UpdatedAt = @event.Timestamp
+        };
+    }
+    
+    public Occasion Apply(IEvent<PartyUpdated> @event, Occasion state)
+    {
+        var party = state.Parties
+            .Where(party => party.Id == @event.Data.PartyId)
+            .Select(((party, i) => new { Value = party, Index = i }))
+            .FirstOrDefault();
+
+        if (party is null || party.Index < 0)
+        {
+            return state;
+        }
+
+        state.Parties[party.Index] = party.Value with
+        {
+            Name = @event.Data.Name,
+            UpdatedAt = @event.Timestamp
+        };
+
+        return state with
+        {
+            UpdatedAt = @event.Timestamp
+        };
+    }
+
+    #endregion
 }
